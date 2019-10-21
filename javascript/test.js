afef2c6037649b023589599fea02155d11235a88
RiverRobins <rivergrahamrobins@gmail.com>
2019-10-21 16:59:24 -0500

    added objects for player and ene

--- javascript/test.js
+++ javascript/test.js
@@ -1,13 +1,89 @@
+"use strict";
+var player;
+//stores all playable character stats
 var characters = [
-    {},
-    {},
-    {},
-    {},
-    {}
+    {
+        name: "jw",
+        hp: 120,
+        def: 0,
+        attacks: {
+            basic: {
+                dmg: 30,
+                skill: 10
+            },
+            special: {
+                dmg: 90,
+                skill: 5
+            }
+        },
+        inventory: []
+    },
+    {
+        name: "kellsey",
+        hp: 150,
+        def: 0,
+        attacks: {
+            basic: {
+                dmg: 30,
+                skill: 10
+            },
+            special: {
+                dmg: 90,
+                skill: 5
+            }
+        },
+        inventory: []
+    },
 ];
 
-function chooseCharacter(n) {
-    if (n === 1) {
-
+// Stores enemies and bosses stats
+var enemies = [
+    {
+        name: "candy-fiend",
+        hp: 60,
+        def: 0,
+        attacks: {
+            basic: {
+                dmg: 15,
+                skill: 5
+            }
+        }
+    },
+    {
+        name: "candy-zombie",
+        hp: 90,
+        def: 0,
+        attacks: {
+            basic: {
+                dmg: 30,
+                skill: 5
+            }
+        }
+    },
+    {
+        name: "pumpkin-zombie",
+        hp: 90,
+        def: 2,
+        attacks: {
+            basic: {
+                dmg: 15,
+                skill: 5
+            }
+        }
+    },
+    {
+        name: "pumpkin boss",
+        hp: 150,
+        def: 5,
+        attacks: {
+            basic: {
+                dmg: 45,
+                skill: 5
+            }
+        }
     }
+];
+
+function encounter(enemy) {
+
 }
